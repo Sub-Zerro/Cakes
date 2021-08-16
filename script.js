827b1cbc3db7d7bf974d0fa127c8a0e2a4e32391
Sub-Zerro <85824405+Sub-Zerro@users.noreply.github.com>
2021-08-16 20:13:26 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+'use strict';
+
+let img1 = document.createElement("img");
+img1.src = "https://cdn.discordapp.com/attachments/876035186305941524/876370887916523570/IMG_20210815_124038.jpg";
+img1.width = "800";
+let div = document.createElement('div');
+
+document.body.append(div);
+div.append(img1);
+
+//
+
+let img2 = document.createElement("img");
+img2.src = "https://cdn.discordapp.com/attachments/876035186305941524/876370888700878908/IMG_20210815_124029.jpg";
+img2.width = "800";
+let div2 = document.createElement('div');
+
+document.body.append(div2);
+div.append(img2);
+
+//
+
+let img3 = document.createElement("img");
+img3.src = "https://cdn.discordapp.com/attachments/876035186305941524/876370889246130206/IMG_20210815_124021.jpg";
+img3.width = "800";
+let div3 = document.createElement('div');
+
+document.body.append(div3);
+div.append(img3);
+
+
+// https://cdn.discordapp.com/attachments/876035186305941524/876777853960749126/IMG_20210816_153705.jpg
+
+
+let img4 = document.createElement("img");
+img4.src = "https://cdn.discordapp.com/attachments/876035186305941524/876777853960749126/IMG_20210816_153705.jpg";
+img4.width = "800";
+let div4 = document.createElement('div');
+
+document.body.append(div4);
+div.append(img4);
